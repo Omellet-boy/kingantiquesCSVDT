ba237c358fd5b23ed26ad1bd9eec85f8c56a8edc
Omellet-boy <75056735+Omellet-boy@users.noreply.github.com>
2025-06-30 14:12:06 +0800

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Product = require("../models/product");
+const Product = require("../models/Product");
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
