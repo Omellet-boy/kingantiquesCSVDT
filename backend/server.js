5b8ecde32fe0eebeceed8927821c8179fdf038c5
Omellet-boy <75056735+Omellet-boy@users.noreply.github.com>
2025-06-30 13:13:21 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,6 @@ const adminRoutes = require("./routes/adminRoutes");
 app.use("/api/products", productRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.listen(5000, () => console.log("✅ Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
